6fec74787cb6fa05b66f23917dfa5d3ca5005af0
Данил Глазунов <danil_glazunov_2100@mail.ru>
2024-10-20 23:48:50 +0300

    данл

--- source/script.js
+++ source/script.js
@@ -1,12 +1,7 @@
-let navMain = document.querySelector('.page-nav');
-let navToggle = document.querySelector('.page-nav__toggle');
+let pageNav = document.querySelector('.page-nav-header');
+let pageToggle = document.querySelector('.page-nav__toggle');
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('toggle-menu--close')) {
-    navMain.classList.remove('toggle-menu--close');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('toggle-menu--close');
-    navMain.classList.remove('main-nav--opened');
-  }
-});
+pageToggle.onclick = function(){
+  pageNav.classList.toggle('page-nav-toggle');
+};
+
